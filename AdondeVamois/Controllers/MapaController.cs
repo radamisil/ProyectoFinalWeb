dfcb262b7cccd297b3d2f99cbc841cd4e04b4615
Rama <radamisil@users.noreply.github.com>
2019-06-01 04:54:01 -0300

    LOGUEO ok SESSION ok

---
+++ AdondeVamois/Controllers/MapaController.cs
@@ -0,0 +1,23 @@
+﻿using AdondeVamos.Models.Entities;
+using AdondeVamos.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdondeVamos.Controllers
+{
+    public class MapaController : Controller
+    {
+        static Usuarios usuarioLogueado = new Usuarios();        
+        private readonly UsuarioServicio _usuarioServicio = new UsuarioServicio();
+        // GET: Mapa
+
+        [HttpGet]
+        public ActionResult Mapa()
+        {
+            return View();
+        }
+    }
+}
